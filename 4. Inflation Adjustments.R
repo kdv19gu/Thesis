fd3fccf55b5dfaeba6c55fb8b1ea23840d1c0113
kdv19gu <kdv19@georgtown.edu>
2025-01-24 17:50:28 -0500

    completed inflation adjustments

--- 4. Inflation Adjustments.R
+++ 4. Inflation Adjustments.R
@@ -66,10 +66,48 @@ county_acs_adj_minwage <- county_acs_minwage |>
             by = c("state_fips", "year")) |> 
   rename(cpi = Annual) |> 
   mutate(r_prevailing_minwage = prevailing_minwage * (case_when(
-    region == "South" ~ 211.338,
-    region == "Northeast" ~ 233.868,
-    region == "West" ~ 221.202,
-    region == "Midwest" ~ 208.046) / cpi)) |> 
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_lowest_quintile = lowest_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_second_quintile = second_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_third_quintile = third_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_fourth_quintile = fourth_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_highest_quintile = highest_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_top_five_percent = top_five_percent * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_median_housing_costs = median_housing_costs * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    p50_p10 = r_third_quintile - r_lowest_quintile,
+    p90_p50 = r_highest_quintile - r_third_quintile,
+    p90_p10 = r_highest_quintile - r_lowest_quintile) |> 
   group_by(statecounty_fips) |> 
   arrange(year) |> 
   mutate(r_prevailing_minwage_diff = r_prevailing_minwage - lag(r_prevailing_minwage),
@@ -85,10 +123,48 @@ city_acs_adj_minwage <- city_acs_minwage |>
             by = c("state_fips", "year")) |> 
   rename(cpi = Annual) |> 
   mutate(r_prevailing_minwage = prevailing_minwage * (case_when(
-    region == "South" ~ 211.338,
-    region == "Northeast" ~ 233.868,
-    region == "West" ~ 221.202,
-    region == "Midwest" ~ 208.046) / cpi)) |> 
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_lowest_quintile = lowest_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_second_quintile = second_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_third_quintile = third_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_fourth_quintile = fourth_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_highest_quintile = highest_quintile * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_top_five_percent = top_five_percent * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    r_median_housing_costs = median_housing_costs * (case_when(
+      region == "South" ~ 211.338,
+      region == "Northeast" ~ 233.868,
+      region == "West" ~ 221.202,
+      region == "Midwest" ~ 208.046) / cpi),
+    p50_p10 = r_third_quintile - r_lowest_quintile,
+    p90_p50 = r_highest_quintile - r_third_quintile,
+    p90_p10 = r_highest_quintile - r_lowest_quintile) |> 
   group_by(statecounty_fips) |> 
   arrange(year) |> 
   mutate(r_prevailing_minwage_diff = r_prevailing_minwage - lag(r_prevailing_minwage),
@@ -104,12 +180,22 @@ rm(list = setdiff(ls(), c("county_acs_adj_minwage", "city_acs_adj_minwage")))
 ### Downloading dataframes as STATA files
 
 county_acs_adj_minwage |> 
+  mutate(state_fips = as.numeric(state_fips),
+         statecounty_fips = as.numeric(statecounty_fips)) |> 
   write_dta("STATA Files/county_acs_adj_minwage.dta")
 
 city_acs_adj_minwage |> 
+  mutate(state_fips = as.numeric(state_fips),
+         statecounty_fips = as.numeric(statecounty_fips),
+         statecountycity_fips = as.numeric(statecountycity_fips)) |> 
   write_dta("STATA Files/city_acs_adj_minwage.dta")
 
-  
+city_acs_adj_minwage |> 
+  filter(state_fips == "06") |> 
+  mutate(state_fips = as.numeric(state_fips),
+         statecounty_fips = as.numeric(statecounty_fips),
+         statecountycity_fips = as.numeric(statecountycity_fips)) |> 
+  write_dta("STATA Files/ca_city_acs_adj_minwage.dta")
   
   
   
